bfea2613a79bc403a9487bfb1baf08f5ce5c5b36
KaiSheng03 <118617185+KaiSheng03@users.noreply.github.com>
2024-12-12 01:28:57 +0800

    Add files via upload

--- models/patient.dart
+++ models/patient.dart
@@ -1,18 +1,24 @@
 class Patient {
   final String name; // Patient's name
   final int id; // Unique identifier
+  final int age; // Age (years)
   final String? email; // Email (optional)
   final String? phoneNumber; // Phone number (optional)
-  final String? dermatoscopyImage;
+  final String? dermatoscopyImage; //dermatoscopy image from gallery or camera
+  final String? diagnosisDate;
+  final String? diagnosisTime;
   final String? diagnosisResult;
 
   // Constructor
   Patient({
     required this.name,
     required this.id,
+    required this.age,
     this.email,
     this.phoneNumber,
     this.dermatoscopyImage,
+    this.diagnosisDate,
+    this.diagnosisTime,
     this.diagnosisResult,
   });
 }
